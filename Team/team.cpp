6d4dc3326490853601538fa58eb25da7cdf1cf4e
Kristina Krajevska <kristina.krajevska@eriden.io>
2022-06-28 12:37:16 +0200

    Team

---
+++ Team/team.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, petya, vasya, tonya;
+    int result=0;
+    cin>>n;
+    for (int i=0;i<n;i++)
+    {
+        cin>>petya>>vasya>>tonya;
+        if(petya+vasya+tonya==2 || petya+vasya+tonya==3 )
+        {
+            result+=1;
+        }
+    }
+
+    cout<<result;
+    return 0;
+
+}
